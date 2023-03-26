a77ca4d71c3f12fcf4e36f985185d3f0283a41a3
hacky <hackyLO@crm.com.tw>
2023-03-26 21:25:38 +0800

    Chore: express and client

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.get('/', (req, res) => {
+    res.send('Hello World From the Typescript Server!');
+});
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+app.use(express_1.default.static(path_1.default.join(__dirname, '../client/build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, './client/build', 'index.html'));
+});
